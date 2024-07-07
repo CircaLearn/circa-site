43e0f2150f59adaf4b6f5be8b8a76c92abc1569f
Hector Astrom <hector.astrom@gmail.com>
2024-07-07 20:04:51 +0200

    + some html skeletons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Page() {
   return (
     <>
-    <p className="text-2xl font-bold">WORDS</p>
+    <p className="text-2xl font-bold">Words</p>
+    <div className="w-96 h-64 mt-6 rounded-2xl shadow-xl bg-white flex flex-col items-center justify-center">
+      <p className="text-center text-xl">Pedantic</p>
+    </div>
     </>
   );
 }
